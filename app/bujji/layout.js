8acfe6383cf6c37355b653ff6937acfeabfd9b21
unknown <srinivasmakam357@gmail.com>
2025-07-12 21:09:21 +0530

    Initial Bujji Chat Room 💖

---
+++ app/bujji/layout.js
@@ -0,0 +1,9 @@
+export default function BujjiLayout({ children }) {
+  return (
+    <html>
+      <body style={{ margin: 0, backgroundColor: '#fff8f8' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
